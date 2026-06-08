4f84753b3df0527d0ca5dfef366914076b3d1002
LimaTheCode <luanfeitoza725@gmail.comgit config --global user.email luanfeitoza725@gmail.com>
2026-06-08 11:31:54 -0300

    aula 05

---
+++ UC1/aula_05/assets/js/scripts.js
@@ -0,0 +1,24 @@
+
+    //variável
+    let nome=" joao lucas"
+    nome= " Minguel"+ nome;
+    nome= "vasco" + nome;
+    //concatenar: juntar use +
+    console.log("nome:"+nome)
+
+    const nomedev = "Caue"
+
+    console.log("Uso constante:" + nomedev)
+    let nome2 = "Allan"
+    let idade = "18"
+    let ecivil = "solteiro"
+    let naturalidade = "fortaleza"
+    let estado = "ceará"
+
+    let dados = "NOME:" + nome2 +
+     "\nIDADE:"+idade+
+     "\nESTADO CIVIL:"+ ecivil+
+     "\nNATURALIDADE:"+ naturalidade+
+     "\nESTADO:"+estado;
+     // \n=quebra de linha
+     console.log(dados)
